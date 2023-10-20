73b6bb33ac14837799c41cb5e60aa1dd109aed2a
Rishi2419 <sahurishikesh1908@gmail.com>
2023-10-21 03:44:33 +0530

    firebase installing & signup activity done

---
+++ app/src/main/java/com/example/assigntodo/auth/SplashActivity.kt
@@ -0,0 +1,24 @@
+package com.example.assigntodo.auth
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.core.os.postDelayed
+import com.example.assigntodo.MainActivity
+import com.example.assigntodo.R
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed(2500) {
+            startActivity(Intent(this, SignupActivity::class.java))
+            finish()
+        }
+    }
+}
