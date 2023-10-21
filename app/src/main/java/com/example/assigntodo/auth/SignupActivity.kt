07c87ca1a6d64cced188fe57318bdbbce90bf24a
Rishi2419 <sahurishikesh1908@gmail.com>
2023-10-21 10:41:33 +0530

    card view for loading done

--- app/src/main/java/com/example/assigntodo/auth/SignupActivity.kt
+++ app/src/main/java/com/example/assigntodo/auth/SignupActivity.kt
@@ -2,11 +2,16 @@ package com.example.assigntodo.auth
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import com.example.assigntodo.R
+import com.example.assigntodo.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySignupBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_signup)
+        binding=ActivitySignupBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
